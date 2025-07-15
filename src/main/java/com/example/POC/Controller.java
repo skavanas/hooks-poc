4c5c93a01f82d3e66ff38392595e98ca8e59988b
skavanas <ennadifanas04@gmail.com>
2025-07-15 16:34:45 +0100

    git-hooks-poc v1

---
+++ src/main/java/com/example/POC/Controller.java
@@ -0,0 +1,12 @@
+package com.example.POC;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
